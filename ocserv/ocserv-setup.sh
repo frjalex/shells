6dde1fd54878ad84687955abb96f52a37310a4ad
weibaohui <zihuxinyu@163.com>
2014-09-11 23:28:31 +0800

    web

--- ocserv/ocserv-setup.sh
+++ ocserv/ocserv-setup.sh
@@ -102,7 +102,12 @@ sysctl -p
 wget -q https://raw.githubusercontent.com/zihuxinyu/shells/master/ocserv/etc/ocserv/ocserv.conf
 mv ocserv.conf /etc/ocserv/
 
-
+####
+# 安装客户端第一次访问时同步的配置文件
+# Download profile.xml file
+####
+wget -q https://raw.githubusercontent.com/zihuxinyu/shells/master/ocserv/etc/ocserv/profile.xml
+mv profile.xml /etc/ocserv/
 
 
 ####
