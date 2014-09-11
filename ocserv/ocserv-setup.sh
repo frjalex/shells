ad68d777e7526da211cc9aa4cf4eb1eaafb2f11c
weibaohui <zihuxinyu@163.com>
2014-09-12 00:12:32 +0800

    web

--- ocserv/ocserv-setup.sh
+++ ocserv/ocserv-setup.sh
@@ -7,8 +7,7 @@ yum update -y
 yum install -y autoconf gnutls-utils automake gcc gmp-devel bison flex  pcre-devel tar net-tools  openssl openssl-devel curl-devel bind-utils  libtasn1-devel zlib zlib-devel trousers trousers-devel gmp-devel gmp xz texinfo libnl-devel libnl tcp_wrappers-libs tcp_wrappers-devel tcp_wrappers dbus dbus-devel ncurses-devel pam pam-devel readline-devel bison bison-devel flex gcc automake autoconf wget
 clear
 
-mkdir autogen
-cd autogen
+
 
 wget http://www.lysator.liu.se/~nisse/archive/nettle-2.7.tar.gz
 tar xvf nettle-2.7.tar.gz
@@ -45,6 +44,8 @@ cd ocserv-0.8.4
 export LD_LIBRARY_PATH=/opt/lib/:/opt/lib64/
 LIBGNUTLS_CFLAGS="-I/opt/include/" LIBGNUTLS_LIBS="-L/opt/lib/ -lgnutls" LIBNL3_CFLAGS="-I/opt/include" LIBNL3_LIBS="-L/opt/lib/ -lnl-3 -lnl-route-3" ./configure --prefix=/opt/
 make && make install&&cd ..
+
+
 
 ####
 # 安装PAM 认证
@@ -88,26 +89,20 @@ mv ocserv /etc/pam.d/
 
 echo "export LD_LIBRARY_PATH=/opt/lib/:/opt/lib64/">> /etc/profile
 echo "export PATH=$PATH:/opt/bin">> /etc/profile
+source /etc/profile
 
-
-
+rm -rf  /etc/ocserv
+mkdir /etc/ocserv
 
 ####
 # 证书，没有就生产
 #
 ####
 
-#服务器的证书和key文件，放在本脚本的同目录下，key文件的权限应该是600或者400
-servercert=server-cert.pem
-serverkey=server-key.pem
 
-#检测是否有证书和key文件
-cd ..
-if [[ ! -f "$servercert" ]] || [[ ! -f "$serverkey" ]]; then
-    #创建ca证书和服务器证书（参考http://www.infradead.org/ocserv/manual.html#heading5）
-    certtool --generate-privkey --outfile ca-key.pem
+certtool --generate-privkey --outfile ca-key.pem
 
-    cat << _EOF_ >ca.tmpl
+cat << _EOF_ >ca.tmpl
 cn = "8dat.com VPN"
 organization = "8dat.com"
 serial = 1
@@ -120,11 +115,11 @@ _EOF_
 
     certtool --generate-self-signed --load-privkey ca-key.pem \
     --template ca.tmpl --outfile ca-cert.pem
-    certtool --generate-privkey --outfile $serverkey
+    certtool --generate-privkey --outfile server-key.pem
 
-    cat << _EOF_ >server.tmpl
+cat << _EOF_ >server.tmpl
 cn = "8dat.com VPN"
-o = "stunnel"
+organization  = "8dat.com"
 serial = 2
 expiration_days = 3650
 signing_key
@@ -132,13 +127,13 @@ encryption_key #only if the generated key is an RSA one
 tls_www_server
 _EOF_
 
-    certtool --generate-certificate --load-privkey $serverkey \
+    certtool --generate-certificate --load-privkey server-key.pem \
     --load-ca-certificate ca-cert.pem --load-ca-privkey ca-key.pem \
-    --template server.tmpl --outfile $servercert
-fi
+    --template server.tmpl --outfile server-cert.pem
+
 
 #把证书复制到ocserv的配置目录
-cp "$servercert" /etc/ocserv/ && cp "$serverkey" /etc/ocserv/
+cp server-cert.pem /etc/ocserv/ && cp server-key.pem /etc/ocserv/
 
 
 
@@ -157,14 +152,14 @@ sysctl -p
 # Download config file
 ####
 wget -q https://raw.githubusercontent.com/zihuxinyu/shells/master/ocserv/etc/ocserv/ocserv.conf
-mv ocserv.conf /etc/ocserv
+mv ocserv.conf /etc/ocserv/
 
 ####
 # 安装客户端第一次访问时同步的配置文件
 # Download profile.xml file
 ####
 wget -q https://raw.githubusercontent.com/zihuxinyu/shells/master/ocserv/etc/ocserv/profile.xml
-mv profile.xml /etc/ocserv
+mv profile.xml /etc/ocserv/
 
 
 ####
@@ -180,4 +175,4 @@ iptables -I FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
 iptables -t nat -A POSTROUTING -s 192.168.10.0/24 -o venet0 -j SNAT --to `ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk 'NR==1 { print $1}'`
 
 
-# service iptables save
+service iptables save
