767ad591669fadf28e859c44530f2b92c077684a
weibaohui <zihuxinyu@163.com>
2014-09-11 21:07:30 +0800

    add gitignore

--- base.sh
+++ base.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-yum install -y m2crypto python-setuptools gcc-c++ tcl unzip nano
+yum install -y m2crypto python-setuptools gcc-c++ tcl unzip nano wget
 easy_install pip
 pip install supervisor
