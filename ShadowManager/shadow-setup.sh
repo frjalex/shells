753480cde3930bea195796ddbd9bf2f7a153dd00
weibaohui <zihuxinyu@163.com>
2014-09-11 21:59:19 +0800

    web

--- ShadowManager/shadow-setup.sh
+++ ShadowManager/shadow-setup.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+yum install -y m2crypto python-setuptools gcc-c++ tcl unzip nano
+easy_install pip
+pip install supervisor
+
+rm -rf /var/proxy
 mkdir /var/proxy
 mkdir /var/proxy/web
 
@@ -14,3 +19,81 @@ cd /var/proxy/web/
 wget https://raw.githubusercontent.com/zihuxinyu/shells/master/ShadowManager/web.zip  --no-check-certificate
 unzip web.zip
 rm -f web.zip
+
+#设置本机IP地址
+ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'
+echo '请输入本机公网IP:'
+read ip
+echo $ip
+
+#采用本机公网绑定，防止代理使用127.0.0.1访问本机资源
+rm -f  /var/proxy/web/conf/app.conf
+echo "appname = ShadowManage">>/var/proxy/web/conf/app.conf
+echo "httpport = 5009">>/var/proxy/web/conf/app.conf
+echo "runmode = prod">>/var/proxy/web/conf/app.conf
+echo "autorender=false">>/var/proxy/web/conf/app.conf
+echo "enablegzip=true">>/var/proxy/web/conf/app.conf
+echo "httpaddr="$ip>>/var/proxy/web/conf/app.conf
+
+
+#加入Supervisord配置
+rm -f /etc/supervisord.conf
+cat >/etc/supervisord.conf<<EOF
+
+;/etc/supervisord.conf
+[unix_http_server]
+file = /var/run/supervisor.sock
+chmod = 0777
+chown= root:root
+
+
+[inet_http_server]
+# Web管理界面设定
+port=127.0.0.1:5008
+#此处改为本机IP地址，防止localhost使用
+username = dabao
+password = dabao
+
+
+[supervisorctl]
+; 必须和'unix_http_server'里面的设定匹配
+serverurl = unix:///var/run/supervisor.sock
+[supervisord]
+logfile=/var/log/supervisord.log ; (main log file;default $CWD/supervisord.log)
+logfile_maxbytes=50MB       ; (max main logfile bytes b4 rotation;default 50MB)
+logfile_backups=1          ; (num of main logfile rotation backups;default 10)
+loglevel=info               ; (log level;default info; others: debug,warn,trace)
+pidfile=/var/run/supervisord.pid ; (supervisord pidfile;default supervisord.pid)
+nodaemon=true              ; (start in foreground if true;default false)
+minfds=1024                 ; (min. avail startup file descriptors;default 1024)
+minprocs=200                ; (min. avail process descriptors;default 200)
+user=root                 ; (default is current user, required if root)
+childlogdir=/var/log/            ; ('AUTO' child log dir, default $TEMP)
+[rpcinterface:supervisor]
+supervisor.rpcinterface_factory = supervisor.rpcinterface:make_main_rpcinterface
+
+
+[program:ssweb]
+command=/var/proxy/web/ShadowManage
+process_name=ssweb
+numprocs=1
+numprocs_start=1
+autostart = true
+startsecs=5
+user=root
+redirect_stderr=true
+
+
+[program:ssserver]
+command=/var/proxy/server
+process_name=ssserver
+numprocs=1
+numprocs_start=1
+autostart = true
+startsecs=5
+user=root
+redirect_stderr=true
+EOF
+
+sed -i "s/127.0.0.1/$ip/g" /etc/supervisord.conf
+nano /etc/supervisord.conf
