3d2a4e9445aab1a74a7e55c1818afca2bf639825
weibaohui <zihuxinyu@163.com>
2014-09-11 21:11:06 +0800

    web

--- ShadowManager/shadow-setup.sh
+++ ShadowManager/shadow-setup.sh
@@ -3,9 +3,9 @@ mkdir /var/proxy
 mkdir /var/proxy/web
 
 cd /var/proxy/web/
-wget https://github.com/zihuxinyu/go/blob/master/src/ShadowManage/linux_x86/ShadowManage  --no-check-certificate
+wget https://raw.githubusercontent.com/zihuxinyu/shells/master/ShadowManager/ShadowManage  --no-check-certificate
 chmod +x ShadowManage
 
 cd /var/proxy/
-wget https://github.com/zihuxinyu/go/blob/master/src/ShadowManage/linux_x86/server  --no-check-certificate
+wget https://raw.githubusercontent.com/zihuxinyu/shells/master/ShadowManager/server  --no-check-certificate
 chmod +x server
