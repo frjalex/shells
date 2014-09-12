f83396a71812df6d5154eb1061f05b8199a20a1f
weibaohui <zihuxinyu@163.com>
2014-09-12 15:35:12 +0800

    web

--- base.sh
+++ base.sh
@@ -17,10 +17,19 @@ chmod +x shadow-setup.sh
 
 
 #####
-# Cisco anyconnect 安装
+# Cisco anyconnect 安装，默认pam radius
 #####
 wget -q  https://raw.githubusercontent.com/zihuxinyu/shells/master/ocserv/ocserv-setup.sh
 chmod +x ocserv-setup.sh
 ./ocserv-setup.sh
 
+
+
+
+#####
+# Freeradius 服务端 安装
+#####
+wget -q  https://raw.githubusercontent.com/zihuxinyu/shells/master/freeradius-serverside/freeradius-setup.sh
+chmod +x freeradius-setup.sh
+./freeradius-setup.sh
 
