3c6a73d2f5f9f2ba11a952067f4227652495eb41
weibaohui <zihuxinyu@163.com>
2014-09-12 14:40:17 +0800

    web

--- freeradius-serverside/freeradius-setup.sh
+++ freeradius-serverside/freeradius-setup.sh
@@ -8,6 +8,11 @@ yum install -y freeradius freeradius-mysql freeradius-utils
 chkconfig radiusd on
 clear
 
+
+
+#####
+#安装mysql
+#####
 echo "安装mysql"
 #todo:设置为unix文件连接
 yum -y install mysql mysql-server mysql-devel
@@ -17,3 +22,12 @@ echo "请输入mysql root 密码："
 read password
 mysqladmin -u root password $password
 clear
+
+
+#####
+#配置freeradius
+#####
+rm -f  /etc/raddb/radiusd.conf
+wget -q https://raw.githubusercontent.com/zihuxinyu/shells/master/freeradius-serverside/etc/raddb/radiusd.conf
+mv radiusd.conf /etc/raddb/
+
